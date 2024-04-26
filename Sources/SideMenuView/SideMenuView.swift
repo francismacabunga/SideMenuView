28fc05b9864f32b94defb30c995d67bba0d62335
francismacabunga <francisnormanmacabunga@gmail.com>
2024-04-26 14:21:33 +0800

    Initial Commit

---
+++ Sources/SideMenuView/SideMenuView.swift
@@ -0,0 +1,40 @@
+//
+//  SideMenuView.swift
+//  TestProject
+//
+//  Created by Macabunga, Francis Norman (Cognizant) on 4/26/24.
+//
+
+import SwiftUI
+
+public struct SideMenuView<mainView: View,
+                           sideMenuView: View>: View {
+    private var MainView: mainView
+    private var SideMenuView: sideMenuView
+    @Binding private var barIsShown: Bool
+    
+    public init(MainView: mainView, 
+                SideMenuView: sideMenuView,
+                barIsShown: Binding<Bool>) {
+        self.MainView = MainView
+        self.SideMenuView = SideMenuView
+        self._barIsShown = barIsShown
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                MainView
+                    .frame(width: geometry.size.width,
+                           height: geometry.size.height)
+                    .offset(x: barIsShown ? geometry.size.width / 2 : 0)
+                
+                if barIsShown {
+                    SideMenuView
+                        .frame(width: geometry.size.width / 2)
+                        .transition(.move(edge: .leading))
+                }
+            }
+        }
+    }
+}
